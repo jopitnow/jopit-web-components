f73b7bd3df83df3608c059d62625fa4bf3adaa6e
Guido Simón Perre <perreguido@gmail.com>
2023-05-10 18:05:24 -0300

    addings success snackbar

--- src/input/password/PasswordTextInput.js
+++ src/input/password/PasswordTextInput.js
@@ -7,23 +7,23 @@ export default function PasswordTextInput(props) {
         <div className="Password_Text_Input_Container">
             <p className="Password_Text_Input_Title">{props.title}</p>
             <input
-                id="Password_Text_Input_Text"
+                id={"password_input_" + props.id}
                 className="Password_Text_Input_Text"
                 type="password"
                 placeholder={props.placeholder}
                 value={props.value}
                 onChange={props.onTextChange}/>
-            <button className="Password_Text_Input_Suffix_Button" onClick={onHideStateChange}>
+            <button className="Password_Text_Input_Suffix_Button" onClick={() => onHideStateChange(props.id)}>
                 <img className="Password_Text_Input_Suffix_Icon" src={suffixIcon} alt="Ocultar/Mostrar contraseña"/>
-                <div id="Password_Text_Input_Suffix_Icon_Line" className="Password_Text_Input_Suffix_Icon_Line"/>
+                <div id={"password_line_" + + props.id} className="Password_Text_Input_Suffix_Icon_Line"/>
             </button>
         </div>
     )
 }
 
-function onHideStateChange() {
-    const input = document.getElementById("Password_Text_Input_Text");
-    const line = document.getElementById("Password_Text_Input_Suffix_Icon_Line");
+function onHideStateChange(id) {
+    const input = document.getElementById("password_input_" + id);
+    const line = document.getElementById("password_line_" + id);
 
     if (input.type === "password") {
         input.type = "text";
