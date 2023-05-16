24392e8463ae4b87f553f551751f9ae263e0c465
Guido Simón Perre <perreguido@gmail.com>
2023-05-16 19:43:29 -0300

    change header css name

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,17 +1,17 @@
 import React from "react";
-import './Header.css';
+import './ShopHeader.css';
 import backIcon from "../../assets/images/header/jopit_back.png";
 
 export default function Header(props) {
     return (
-        <div className="Header">
-            <p className="Header_Title">{props.title}</p>
-            <p className="Header_Description">{props.description}</p>
+        <div className="Shop_Header">
+            <p className="Shop_Header_Title">{props.title}</p>
+            <p className="Shop_Header_Description">{props.description}</p>
             {
                 props.isBackEnabled &&
-                    <button className="Header_Back_Container" onClick={() => history.back()}>
-                        <img className="Header_Back_Image" src={backIcon} alt="Volver"/>
-                        <p className="Header_Back_Text">Volver</p>
+                    <button className="Shop_Header_Back_Container" onClick={() => history.back()}>
+                        <img className="Shop_Header_Back_Image" src={backIcon} alt="Volver"/>
+                        <p className="Shop_Header_Back_Text">Volver</p>
                     </button>
             }
         </div>
