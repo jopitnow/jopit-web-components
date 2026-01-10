d89adf786e05f20b8247397ee2fab32038409b5e
Guido Simón Perre <perreguido@gmail.com>
2026-01-10 18:02:03 -0300

    [IMPROVE] Adjust Name

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "index.js",
-        library: "@jopit/web-components",
+        library: "@jopitnow/web-components",
         libraryTarget: "umd",
         globalObject: "this"
     },
